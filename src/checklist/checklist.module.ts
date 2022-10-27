7243de269cf9392bccd5704f088b18c416af5dfb
mismaah <mismaahabdulla@gmail.com>
2022-10-27 09:28:34 +0500

    feat(checklist): rest endpoints for pms

--- src/checklist/checklist.module.ts
+++ src/checklist/checklist.module.ts
@@ -3,10 +3,17 @@ import { ChecklistService } from './checklist.service';
 import { ChecklistResolver } from './checklist.resolver';
 import { ChecklistTemplateModule } from 'src/resolvers/checklist-template/checklist-template.module';
 import { EntityModule } from 'src/entity/entity.module';
+import { ChecklistController } from './checklist.controller';
+import { AuthModule } from 'src/resolvers/auth/auth.module';
 
 @Module({
   providers: [ChecklistResolver, ChecklistService],
-  imports: [ChecklistTemplateModule, forwardRef(() => EntityModule)],
+  imports: [
+    ChecklistTemplateModule,
+    forwardRef(() => EntityModule),
+    AuthModule,
+  ],
   exports: [ChecklistService],
+  controllers: [ChecklistController],
 })
 export class ChecklistModule {}
