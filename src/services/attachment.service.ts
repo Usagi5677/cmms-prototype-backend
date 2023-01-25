5b8a490267cd33996d3bc5b091fcab44329ee23e
mismaah <mismaahabdulla@gmail.com>
2023-01-25 11:08:52 +0500

    feat(sharepoint): log errors

--- src/services/attachment.service.ts
+++ src/services/attachment.service.ts
@@ -120,6 +120,7 @@ export class AttachmentService {
         })
       );
     } catch (e) {
+      this.logger.error(`Sharepoint: ${e}`);
       throw new NotFoundException('File not found');
     }
   }
